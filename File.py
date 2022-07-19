269552deb6603b0a690c4db0fbedbdda0201a743
RINKU-CYBER-404 <109244698+RINKU-CYBER-404@users.noreply.github.com>
2022-07-19 09:15:06 +0600

    Update File.py

--- File.py
+++ File.py
@@ -1338,4 +1338,50 @@ def Subscraption():
 		os.system('am start https://wa.me/+923111908699?text=' + tks)
 		Subscraption()        
 Main()
+else:
 
+		os.system("clear")		print(logo)
+
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+
+		time.sleep(1)
+
+		os.system("clear")
+
+		print(logo)
+
+		print ("")
+
+		print(" \033[1;32m The Rinku You Need To Get Approval First\033[1;37m\n")
+
+		print(" \033[1;32m Note : THIS TOOL YOU CAN USE IT FREE \033[1;37m")
+
+		print ("")
+
+		print(" Your Key is Not Approved ")
+
+		print("")
+
+		print(" Copy And Send Key To Admin")
+
+		print ("")
+
+		print (" Your Key : "+ak+rinku+key1)
+
+		print ("")
+
+		name = input(" Your Name : ")
+
+		print ("")
+
+		input(" Press Enter To Send Key")
+
+		time.sleep(3.5)
+
+		tks = 'Dear%20Rinku,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+rinku+''+key1
+
+		os.system('am start https://wa.me/+8801915489551?text=' + tks)
+
+		Subscraption()        
+
+Subscraption()
